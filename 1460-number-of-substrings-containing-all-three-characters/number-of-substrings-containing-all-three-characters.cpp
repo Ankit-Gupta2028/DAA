698853b58e8bc4161fc5a9ade5877535ef29b652
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2026-02-26 01:00:06 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (94.81%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -4,10 +4,12 @@ public:
         int store[3]={-1,-1,-1};
         int Num_of_Substrings=0;
         for(int i=0;i<s.size();i++){
+
             store[s[i]-'a']=i;
-            if(store[0]!=-1 && store[1]!=-1 && store[2]!=-1 ){
-                Num_of_Substrings+= 1 + min({store[0],store[1],store[2]});
-            }
+             Num_of_Substrings+= 1 + min({store[0],store[1],store[2]});
+            // if(store[0]!=-1 && store[1]!=-1 && store[2]!=-1 ){
+            //     Num_of_Substrings+= 1 + min({store[0],store[1],store[2]});
+            // }
         }
         return Num_of_Substrings;
     }
