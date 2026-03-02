617e63a1689304e685422770150b528b4fe6f13c
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2026-03-02 17:52:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (54.28%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> stack_operations;
+        if(target.size()==0){
+            return stack_operations;
+        }
+        int check_num=1;
+        int index=0;
+
+        
+
+        while(index<target.size()){
+            if(target[index] == check_num){
+                stack_operations.push_back("Push");
+                
+                index+=1;
+            }else{
+                stack_operations.push_back("Push");
+                stack_operations.push_back("Pop");
+            }
+            
+            check_num+=1;
+        }
+        return stack_operations;
+    }
+};
