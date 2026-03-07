b1c02a34835dc29a57fca038f91708ed7907c791
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2026-03-07 16:38:04 +0530

    Time: 2 ms (2.29%) | Memory: 9.8 MB (30.84%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -6,39 +6,45 @@ public:
     }
     
     void push(int x) {
-        while(!s1.empty()){
-            s2.push(s1.top());
-            s1.pop();
-        }
         s1.push(x);
-        
-        while(!s2.empty()){
-            s1.push(s2.top());
-            s2.pop();
-        }
-
     }
     
     int pop() {
-        if(s1.size()==0){
+        if(s2.empty()){
+            
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+            
+        }
+        if(s2.empty()){
             cout<<"Underflow";
             return -1;
         }
-       int popped=s1.top();
-       s1.pop();
-       return popped;
+        int popped= s2.top();
+        s2.pop();
+        return popped;
     }
     
     int peek() {
-        if(s1.size()==0){
+        if(s2.empty()){
+            
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+            
+        }
+        if(s2.size()==0){
             cout<<"Queue is Empty";
             return -1;
         }
-        return s1.top();
+        return s2.top();
     }
     
     bool empty() {
-        return (s1.size()==0);
+        return (s1.empty() && s2.empty());
     }
 };
 
