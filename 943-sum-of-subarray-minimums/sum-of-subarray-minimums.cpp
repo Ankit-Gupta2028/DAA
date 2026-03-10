35ebdcc9ecab133628dd96e3c2210e51f800c025
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2026-03-11 00:38:40 +0530

    Time: 16 ms (58.04%) | Memory: 47.3 MB (21.03%) - LeetSync

---
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,59 @@
+class Solution {
+private:
+    vector<int> NSE(vector<int> &arr){
+        int n=arr.size();
+        vector<int> v(n);
+        stack<int> s1;
+
+        for(int i=n-1;i>=0;i--){
+            int curr=arr[i];
+            while(!s1.empty() && curr <= arr[s1.top()]){
+                s1.pop();
+            }
+            if(!s1.empty()){
+                v[i]=s1.top();
+            }else{
+                v[i]=n;
+            }
+            s1.push(i);
+        }
+        return v;
+    }
+    vector<int> PSE(vector<int> &arr){
+        int n=arr.size();
+        vector<int> v(n);
+        stack<int> s1;
+
+        for(int i=0;i<n;i++){
+            int curr=arr[i];
+            while(!s1.empty() && curr < arr[s1.top()]){
+                s1.pop();
+            }
+            if(!s1.empty()){
+                v[i]=s1.top();
+            }else{
+                v[i]= -1;
+            }
+            s1.push(i);
+        }
+        return v;
+    }
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int mod=1e9 +7;
+        int n=arr.size();
+        vector<int> pse=PSE(arr);
+        vector<int> nse=NSE(arr);
+        int sum=0;
+
+        for(int i=0;i<n;i++){
+            int left=i-pse[i];
+            int right=nse[i]-i;
+            long long freq=left*right*1LL;
+            int curr_sum=(freq*arr[i]*1LL)%mod;
+            sum=(sum+curr_sum)%mod;
+        }
+        return sum;
+        
+    }
+};
