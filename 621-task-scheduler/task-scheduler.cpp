ec20ba83f23a6c1932f4c03cf14e1335dca87eba
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2026-04-01 23:06:10 +0530

    Time: 43 ms (22.37%) | Memory: 47.9 MB (10.99%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        if(n==0){
+            return tasks.size();
+        }
+
+       unordered_map<int,int> arr;
+
+        priority_queue<int> p1;
+        
+        for(int i:tasks){
+            arr[i-'A']+=1;
+        }
+        for(auto &x: arr){
+            p1.push(x.second);
+        }
+
+        
+        int ans=0;
+         
+
+        while(!p1.empty()){
+            vector<int> store;
+            int count=0;
+            for(int i=0;i<n+1;i++){
+                if(!p1.empty()){
+                    int freq=p1.top();
+                    p1.pop();
+                    freq--;
+                    
+                    store.push_back(freq);
+                    
+
+                }
+
+            }
+            for(int &i: store){
+                if(i>0){
+                    p1.push(i);
+                }
+                
+            }
+            if(p1.empty()){
+                ans+=store.size();
+            }else{
+                ans+=n+1;
+            }
+  
+        }
+        return ans;
+    }
+};
