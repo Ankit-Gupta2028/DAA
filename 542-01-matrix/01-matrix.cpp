ed3ae78507c86442f9147ecc8a4f8e2497544418
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2026-06-01 21:57:40 +0530

    Time: 16 ms (64.98%) | Memory: 38.9 MB (29.86%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,26 +1,26 @@
 class Solution {
 private:
     bool isvaild(int i,int j,int n,int m){
-        if(i<0 || i>=n){
-            return false;
-        }
-        if(j<0 || j>=m){
-            return false;
-        }
-        return true;
+    if(i<0 || i>=n){
+        return false;
+    }
+    if(j<0 || j>=m){
+        return false;
+    }
+    return true;
     
     }
-
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-         int n=mat.size();
+          int n=mat.size();
         int m=mat[0].size();
-        
+
+        vector<vector<int>> ans(n,vector<int>(m,0));
+        vector<vector<int>> vis(n,vector<int>(m,0));
+
         int drow[]={-1,0,1,0};
         int dcol[]={0,1,0,-1};
 
-        vector<vector<int>> ans(n,vector<int>(m,0));
-        vector<vector<int>> visited(n,vector<int>(m,0));
         queue<pair<int,pair<int,int>>> q1;
 
 
@@ -28,35 +28,33 @@ public:
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
                     q1.push({0,{i,j}});
-                    visited[i][j]=1;
-                }
-                
+                    vis[i][j]=1;
+                }    
             }
         }
 
         while(!q1.empty()){
             auto ele=q1.front();
-            auto dis=ele.first;
-            auto row=ele.second.first;
-            auto col=ele.second.second;
             q1.pop();
-
+            int dis=ele.first;
+            int row=ele.second.first;
+            int col = ele.second.second;
             ans[row][col]=dis;
 
             for(int i=0;i<4;i++){
-                    int newrow = row+drow[i];
-                    int newcol = col+dcol[i];
-                    if(isvaild(newrow,newcol,n,m) && visited[newrow][newcol]==0){
+                int newrow=row+drow[i];
+                int newcol=col+dcol[i];
+
+                if(isvaild(newrow,newcol,n,m)){
+                    if(vis[newrow][newcol]==0 ){
                         q1.push({dis+1,{newrow,newcol}});
-                        visited[newrow][newcol]=1;
+                        vis[newrow][newcol]=1;
                     }
+                }
             }
-
-
+        
+        
         }
-
-
-
         return ans;
     }
 };
