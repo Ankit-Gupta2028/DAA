6a6d9c921bb68873d90e4d8582beddaffe99da3e
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2026-07-05 14:53:12 +0530

    Time: 360 ms (21.97%) | Memory: 21.5 MB (29.35%) - LeetSync

---
+++ 127-word-ladder/word-ladder.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        set<string> s1;
+        for(auto word: wordList){
+            s1.insert(word);
+        }
+
+        queue<pair<string,int>> q1;
+        q1.push({beginWord,1});
+
+        while(!q1.empty()){
+            auto ele= q1.front();
+            q1.pop();
+            string word= ele.first;
+            int transformation = ele.second;
+
+            for(int i=0;i<word.size();i++){
+                for(char j='a';j<='z';j++){
+                    string word2=word;
+                    word2[i]=j;
+                    if(s1.find(word2)!= s1.end()){
+                        if(word2 == endWord){
+                            return transformation+1;
+                        }
+                        q1.push({word2,transformation+1});
+                        s1.erase(word2);
+                    }
+                }
+            }
+
+        }
+        return 0;
+    }
+};
