3160ca465594fdd679ac9fae9bd09c0361dc6377
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2026-08-20 01:27:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (88.51%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,20 +1,16 @@
 class Solution {
-int recursion(int n,vector<int> &dp){
 
-    if(n <= 1){
-        return 1;
-    }
-    if(dp[n] != -1 ){
-        return dp[n];
-    }
-
-    return dp[n]=recursion(n-1,dp)+recursion(n-2,dp);
-
-}
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
+        int prev = 1;
+        int prev2 =1 ;
+
+        for(int i=2;i<=n;i++){
+            int curr_i = prev2+prev;
+            prev2 = prev;
+            prev = curr_i;
+        }
         
-        return recursion(n,dp);
+        return prev;
     }
 };
