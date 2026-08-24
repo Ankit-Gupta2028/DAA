dba10b60aad2e068ced865945e00c57c2ce3fdeb
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2026-08-24 20:04:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (31.11%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,30 +1,27 @@
 class Solution {
+int unique_ways(int row,int col,vector<vector<int>> &dp){
 
-int unique_ways(int row,int col,int m,int n,vector<vector<int>> &dp){
-
-    if(row == m-1 && col == n-1 ){
+    if(row == 0 && col == 0 ){
         return 1;
     }
     if(dp[row][col] != -1){
         return dp[row][col];
     }
-    int right = 0;
-    int down = 0;
-    if(col+1 <= n-1){
-         right=unique_ways(row,col+1,m,n,dp);
+    int left = 0;
+    int up = 0;
+    if(col-1 >= 0){
+        left = unique_ways(row,col-1,dp);
     }
-     if(row+1 <= m-1){
-         down = unique_ways(row+1,col,m,n,dp);
+     if(row-1 >= 0){
+        up = unique_ways(row-1,col,dp);
     }
-    return dp[row][col] = right+down;
+    return dp[row][col] = left + up;
 
 }
 public:
     int uniquePaths(int m, int n) {
-        int row = 0;
-        int col = 0;
-  
+        
         vector<vector<int>> dp(m , vector<int>(n,-1));
-        return unique_ways(row,col,m,n,dp);
+        return unique_ways(m-1,n-1,dp);
     }
 };
