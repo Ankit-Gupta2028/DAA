a77edd2cf94e92f6eb84cc8a8cf480a5f99f0742
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2026-08-25 00:59:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (77.81%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -8,27 +8,33 @@ public:
         int ans = 1;
         
         for(int i=0;i<m;i++){
+
+            vector<int> temp(n,0);
+
             for(int j=0;j<n;j++){
+
                 int up = 0;
                 int left = 0;
                 if(i==0 && j==0){
-                    up = dp[0];
-                    left = left_past;
+                    temp[j]=1;
+                    
                     
                 }else{
                     if(i>0){
                         up = dp[j];
                     } 
                     if(j>0){
-                        left = left_past;
+                        left = temp[j-1];
                     }
-                     ans = left+up;
-                    dp[j]=ans;
+                    ans = left+up;
+                    temp[j]=ans;
                     left_past = ans;
                 }
                 
             }
+            dp = temp;
         }
         return ans;
+        
     }
 };
