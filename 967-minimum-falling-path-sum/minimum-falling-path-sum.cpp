77729c0102ff262f74be928b5bb448353fd742c6
Ankit-Gupta2028 <iamankit.9798@gmail.com>
2026-08-27 02:23:26 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (83.83%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,39 +1,46 @@
 class Solution {
-int min_path(vector<vector<int>>& matrix,int row,int col,int curr_row,int curr_col,vector<vector<int>> &dp){
 
-    if(curr_col < 0 || curr_col > col){
-        return 1e9;
-    }
-    if(curr_row == 0 ){
-        return matrix[curr_row][curr_col];
-    }
-
-    if(dp[curr_row][curr_col] != INT_MAX){
-        return dp[curr_row][curr_col];
-    }
-    
-        int up = matrix[curr_row][curr_col]+min_path(matrix,row,col,curr_row-1,curr_col,dp);
-     
-        int up_left = matrix[curr_row][curr_col]+min_path(matrix,row,col,curr_row-1,curr_col-1,dp);
-    
-        int up_right = matrix[curr_row][curr_col]+min_path(matrix,row,col,curr_row-1,curr_col+1,dp);
-    
-
-    return dp[curr_row][curr_col]=min(up,min(up_left,up_right));
-
-    }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         
         int n = matrix.size();
         int m = matrix[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,INT_MAX));
+        vector<int> dp(m,INT_MAX);
+
+        for(int i=0;i<m;i++){
+            dp[i] = matrix[0][i];
+        }
+
+        for(int i=1;i<n;i++){
+            vector<int> temp(m,INT_MAX);
+            for(int j=0;j<m;j++){
+
+                
+                int up_left = INT_MAX;
+                int up_right = INT_MAX;
+
+               
+                int up = matrix[i][j]+dp[j];
+                
+                if( j+1 < m){
+                    up_right = matrix[i][j]+dp[j+1];
+                }
+                if( j-1 >=0){
+                    up_left = matrix[i][j]+dp[j-1];
+                }
+                temp[j] = min(up,min(up_left,up_right));
+                
+            }
+            dp = temp;
+   
+        }
 
         int ans = INT_MAX;
 
-        for(int i=1;i<=m;i++){
-            ans = min(ans,min_path(matrix,n-1,m-1,n-1,m-i,dp));
+        for(int i=0;i<m;i++){
+            ans = min(ans,dp[i]);
         }
+
 
         return ans;
     }
