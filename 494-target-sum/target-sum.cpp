4f5610f34d29216aaad230caf8c5cd567330e355
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-07 21:52:37 +0530

    Time: 6 ms (70.12%) | Memory: 15.1 MB (59.64%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -13,35 +13,37 @@ public:
      if(total_sum-abs(target) < 0 || (total_sum-target) % 2 != 0){
         return 0;
     }
-    vector<vector<int>> dp(n,vector<int>(total_sum+1,0));
-
     int Target = (total_sum - target)/2;
+   vector<int> dp(Target+1,0);
+
+    
 
     if(nums[0]==0){
-        dp[0][0]=2;
+        dp[0]=2;
     }else{
-        dp[0][0]=1;
+        dp[0]=1;
     }
     if(nums[0]<=Target && nums[0] != 0){
-        dp[0][nums[0]]=1;
+        dp[nums[0]]=1;
     }
 
     for(int i=1;i<n;i++){
+        vector<int> temp(Target+1,0);
         for(int target1=0;target1<=Target;target1++){
-            int not_take = dp[i-1][target1];
+            int not_take = dp[target1];
             int take =0;
 
         if(nums[i] <= target1){
-               take=dp[i-1][target1-nums[i]];
+               take=dp[target1-nums[i]];
         }
-        dp[i][target1] = (take+not_take)%module;
+        temp[target1] = (take+not_take)%module;
      
         }
+        dp=temp;
 
     }
 
 
-    return dp[n-1][Target];
-
+    return dp[Target];
     }
 };
