9e875882fe74a1b130dc4d52430176c677f5bcb3
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-07 21:47:21 +0530

    Time: 3 ms (86.17%) | Memory: 17.8 MB (35.26%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,53 +1,47 @@
 class Solution {
     int module = 1e9+7;
-int possible(int index,vector<int>&arr, int target,vector<vector<int>> &dp){
 
-    
-    if(index == 0){
-        if(target == 0 && arr[0]==0){
-            return 2;
-        }
-        if(target == 0 || arr[0]==target){
-            return 1;
-        }
-        return 0;
-      
-    }
-
-    if(dp[index][target] != -1){
-        
-        return dp[index][target] ;
-    }
-    int not_take = possible(index-1,arr,target,dp);
-
-    int take = 0;
-    if(arr[index] <= target){
-        take = possible(index-1,arr,target - arr[index],dp);
-    }
-
-    return dp[index][target] = (take + not_take)%module;
-
-}
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
 
-    int n = nums.size();
     int total_sum = 0;
-    for(auto& num:nums){
-        total_sum+=num;
-    }
-    int Target = (total_sum-target)/2;
+    int n = nums.size();
 
-    if(total_sum - target < 0){
+    for(int i=0;i<n;i++){
+        total_sum+=nums[i];
+    }
+     if(total_sum-abs(target) < 0 || (total_sum-target) % 2 != 0){
         return 0;
     }
+    vector<vector<int>> dp(n,vector<int>(total_sum+1,0));
 
-    if((total_sum - target)%2 == 1){
-        return 0;
+    int Target = (total_sum - target)/2;
+
+    if(nums[0]==0){
+        dp[0][0]=2;
+    }else{
+        dp[0][0]=1;
+    }
+    if(nums[0]<=Target && nums[0] != 0){
+        dp[0][nums[0]]=1;
     }
 
-    vector<vector<int>> dp(n,vector<int>(Target+1,-1));
+    for(int i=1;i<n;i++){
+        for(int target1=0;target1<=Target;target1++){
+            int not_take = dp[i-1][target1];
+            int take =0;
 
-    return possible(n-1,nums,Target,dp);
+        if(nums[i] <= target1){
+               take=dp[i-1][target1-nums[i]];
+        }
+        dp[i][target1] = (take+not_take)%module;
+     
+        }
+
+    }
+
+
+    return dp[n-1][Target];
+
     }
 };
