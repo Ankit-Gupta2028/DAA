012a2fa12d1ca8ffabea00b067bd57dda9e02b98
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-08 19:46:38 +0530

    Time: 39 ms (36.83%) | Memory: 36.3 MB (68.10%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,33 +1,33 @@
 class Solution {
     int module = INT_MAX;
-    int possible(int index,vector<int>&coins, int amount,vector<vector<int>> &dp){
-        
-        if(amount == 0){
-            return 1;
-        }
-        
-        if(index == 0){
-            if(amount%coins[0] == 0){
-                return 1;
-            }else{
-                return 0;
-            }
-        }
-        if(dp[index][amount] != -1){
-            return dp[index][amount];
-        }
-        int not_take = possible(index-1,coins,amount,dp);
-        int take = 0;
-        if(coins[index] <= amount){
-            take = possible(index,coins,amount-coins[index],dp);
-        }
-        return dp[index][amount] = (take + not_take)%module;
-    }
+
 public:
     int change(int amount, vector<int>& coins) {
         int N = coins.size();
-        vector<vector<int>> dp(N,vector<int>(amount+1,-1));
+        vector<vector<int>> dp(N,vector<int>(amount+1,0));
+        for(int i=0;i<=amount;i++){
+             if(i%coins[0] == 0){
+                dp[0][i] = 1;
+            }
+        }
+         for(int i=0;i<N;i++){
+            dp[i][0]= 1;
+        }
 
-        return possible(N-1,coins,amount,dp);
+        
+
+        for(int i=1;i<N;i++){
+            for(int amt = 1; amt<= amount;amt++){
+                long long  not_take = dp[i-1][amt];
+                long long take = 0;
+                if(coins[i] <= amt){
+                    take = dp[i][amt-coins[i]];
+                }
+                dp[i][amt] = (take + not_take)%module;
+            }
+        }
+        return dp[N-1][amount];
+       
+
     }
 };
