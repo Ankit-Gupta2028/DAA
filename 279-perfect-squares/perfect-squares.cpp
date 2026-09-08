db21cc37275c17a0998b403d7feb39d13d165fab
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-08 22:53:30 +0530

    Time: 179 ms (18.83%) | Memory: 162.2 MB (10.61%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -23,9 +23,34 @@ public:
         
 
         int N = sqrt(n);
-        vector<vector<int>> dp(N+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp(N+1,vector<int>(n+1,1e9));
 
-        return least_num(N,dp,n);
+        for(int index = 0; index <= N; index++) {
+            dp[index][0] = 0;
+        }
+
+        
+        for(int target = 1; target <= n; target++) {
+            dp[1][target] = target;
+        }
+
+        for(int index = 2; index <= N; index++){
+            for(int target = 1; target <= n; target++){
+                int not_take = dp[index-1][target];
+
+                int take = 1e9;
+                int square = index*index;
+                if(target >= square){
+                    take = 1+dp[index][target-square];
+                }
+                
+                 dp[index][target] = min(take,not_take);
+            }
+        }
+
+      
+
+        return dp[N][n];
 
     }
 };
