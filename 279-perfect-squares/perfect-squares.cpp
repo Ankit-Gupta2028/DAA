e58caf0aff8a2930c1a1fc33180e46a2f2508946
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-08 22:31:46 +0530

    Time: 229 ms (13.42%) | Memory: 160.2 MB (14.61%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,24 +1,5 @@
 class Solution {
-int least_num( int index,vector<int> &nums,vector<vector<int>> &dp,int target){
-    if( target == 0){
-        return 0;
-    }
-    if(index == 0){
-        if(target % nums[0] == 0){
-            return target / nums[0];
-        }
-        return 1e9;
-    }
-    if(dp[index][target] != -1){
-        return dp[index][target];
-    }
-    int not_take = least_num(index-1,nums,dp,target);
-    int take = 1e9;
-    if(target >= nums[index]){
-        take = 1+least_num(index,nums,dp,target-nums[index]);
-    }
-    return dp[index][target] = min(take,not_take);
-}
+
 public:
     int numSquares(int n) {
         vector<int> nums;
