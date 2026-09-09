a46c419e411aa8caf95f60acb6e9ded81ea261d5
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-09 14:02:54 +0530

    Time: 499 ms (24.32%) | Memory: 296.4 MB (27.54%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,33 +1,29 @@
 class Solution {
-int longest_incr_sub(int index,int prev_index,vector<int>& nums,vector<vector<int>> &dp){
-   
-    if(index == nums.size()-1 ){
-        if(prev_index == -1 || nums[index] > nums[prev_index]){
-            return 1;
-        }
-       
-        return 0;
-    }
-    if(dp[index][prev_index+1] != -1){
-        return dp[index][prev_index+1];
-    }
-    int not_take = 0+longest_incr_sub(index+1,prev_index,nums,dp);
-
-    int take = 0;
-
-    if(prev_index == -1 ){
-        take = longest_incr_sub(index+1,index,nums,dp)+1;
-    }else if(nums[index] > nums[prev_index]){
-        take = longest_incr_sub(index+1,index,nums,dp)+1;
-    }
-    
-    return dp[index][prev_index+1] = max(take,not_take);
-}
 public:
     int lengthOfLIS(vector<int>& nums) {
-         int n = nums.size();
-      vector<vector<int>> dp(n, vector<int>(n+1,-1) );
+          int n = nums.size();
+      vector<vector<int>> dp(n, vector<int>(n+1,0) );
 
-      return longest_incr_sub(0,-1,nums,dp);
+        for(int i=-1;i<n;i++){
+            if( i== -1 || nums[n-1] > nums[i] ){
+                dp[n-1][i+1] = 1;
+            }
+        }
+
+        for(int index=n-2; index>=0;index--){
+            for(int prev_index = -1;prev_index<n;prev_index++){
+                int not_take = dp[index+1][prev_index+1];
+
+                int take = 0;
+                if(prev_index == -1 || nums[index] > nums[prev_index]){
+                    take = 1+ dp[index+1][index+1];
+                }
+                dp[index][prev_index+1] = max(not_take,take);
+
+            }
+        }
+
+
+      return dp[0][0];
     }
 };
