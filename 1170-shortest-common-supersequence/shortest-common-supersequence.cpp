ea31cb5befba3ba3ed272184fabff13c073e983d
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-14 13:45:50 +0530

    Time: 34 ms (6.01%) | Memory: 44.8 MB (5.65%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -8,16 +8,16 @@ class Solution {
 
         for(int index1=1;index1<=s1;index1++){
             for(int index2=1;index2<=s2;index2++){
-                int Equal = 0;
-                int Not_Equal = 0;
+                
+                
                 if(str1[index1-1] == str2[index2-1]){
-                    Equal = 1+dp[index1-1][index2-1];
+                    dp[index1][index2] = 1+dp[index1-1][index2-1];
                 }else{
                     int first = dp[index1-1][index2];
                     int  second = dp[index1][index2-1];
-                     Not_Equal = max(first,second);
+                     dp[index1][index2] = max(first,second);
                 }
-                dp[index1][index2] = max(Equal,Not_Equal);
+                
             }
 
         }
