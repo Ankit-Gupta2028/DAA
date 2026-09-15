73a4d974225b236efc5d8d569f255a22feddb26c
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-16 01:36:25 +0530

    Time: 7 ms (61.41%) | Memory: 13.2 MB (61.20%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,35 +1,33 @@
 class Solution {
-private:
-    int Edit_distance(int index1,int index2,string &start, string &target,  vector<vector<int>> &dp){
-        if(index1 < 0){
-            return index2+1;
-        }
-        if(index2 < 0){
-            return index1+1;
-        }
-        if(dp[index1][index2] != -1){
-            return dp[index1][index2];
-        }
 
-        if(start[index1] == target[index2]){
-            dp[index1][index2]= Edit_distance(index1-1,index2-1,start,target,dp);
-        }else{
-             dp[index1][index2] =  1 + 
-             min(Edit_distance(index1-1,index2,start,target,dp),
-             min(Edit_distance(index1-1,index2-1,start,target,dp),
-             Edit_distance(index1,index2-1,start,target,dp)));
-        }
-        return dp[index1][index2];
-        
-    }
 public:
     int minDistance(string word1, string word2) {
         
         int index1 = word1.size();
         int index2 = word2.size();
 
-        vector<vector<int>> dp(index1,vector<int>(index2,-1));
+      
 
-        return Edit_distance(index1-1,index2-1,word1,word2,dp);
+        vector<vector<int>> dp(index1+1,vector<int>(index2+1,0));
+
+        for(int i=0;i<=index2;i++){
+            dp[0][i] = i;
+        }
+         for(int i=0;i<=index1;i++){
+            dp[i][0] = i;
+        }
+        for(int i=1;i<=index1;i++){
+            for(int j=1;j<=index2;j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j]= dp[i-1][j-1];
+                }else{
+                dp[i][j] =  1 + 
+                min(dp[i-1][j],min(dp[i-1][j-1],
+                dp[i][j-1]));
+                }
+            }
+        }
+        return dp[index1][index2];
+         
     }
 };
