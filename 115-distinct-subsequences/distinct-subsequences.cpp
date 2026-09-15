866dd324a75dd9fba8a2b7252b5caa8595252753
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-15 23:58:53 +0530

    Time: 15 ms (77.41%) | Memory: 8.9 MB (94.67%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -3,33 +3,32 @@ class Solution {
 
 public:
     int numDistinct(string s, string t) {
-       int index1 = s.size();
+         int index1 = s.size();
        int index2 = t.size();
 
-       vector<vector<int>> dp(index1+1,vector<int>(index2+1,0));
+       vector<int> curr (index2+1,0);
+        vector<int> prev (index2+1,0);
 
-        for(int i=0;i<=index1;i++){
-            dp[i][0] = 1;
-        }
-        for(int i=1;i<=index2;i++){
-            dp[0][i] = 0;
-        }
+        
+        curr[0] = 1; 
+        prev[0] = 1;
        
         for(int i=1;i<=index1;i++){
             for(int j = 1;j<=index2;j++){
 
                 if(s[i-1] == t[j-1]){
 
-                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j]) % modeule;
+                    curr[j] = (prev[j-1] + prev[j]) % modeule;
 
                 }else{
 
-                    dp[i][j] = dp[i-1][j];
+                    curr[j] = prev[j];
 
                 }
             }
+            prev = curr;
         }
-        return dp[index1][index2];
+        return curr[index2];
 
     }
 };
