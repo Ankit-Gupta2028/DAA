4855b4f64f8f6d9949210e72da5e034fc4fd3252
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-17 01:31:23 +0530

    Time: 63 ms (31.21%) | Memory: 54.6 MB (23.00%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,46 +1,39 @@
 class Solution {
-bool Wildcard_matching(int index1,int index2,string &str, string &pat, vector<vector<int>> &dp){
-    if(index1 < 0 && index2 < 0){
-        return true;
-    }
-    if(index1 >= 0 && index2 < 0){
-        if(pat[0] == '*'){
-            return true;
-        }
-        return false;
-    }
-    if(index1 < 0 && index2 >=0){
-        while(index2 >=0){
-            if(pat[index2] != '*'){
-                return false;
-            }
-            index2--;
-        }
 
-        return true;
-    }
-    
-
-    if(dp[index1][index2] != -1){
-        return dp[index1][index2];
-    }
-    if(str[index1] == pat[index2] || pat[index2] == '?'){
-        return dp[index1][index2]=Wildcard_matching(index1-1,index2-1,str,pat,dp);
-    }
-    else if(pat[index2] == '*'){
-        return dp[index1][index2] = Wildcard_matching(index1-1,index2,str,pat,dp) || Wildcard_matching(index1,index2-1,str,pat,dp); 
-    }else{
-        return dp[index1][index2] = false;
-    }
-}
 public:
     bool isMatch(string s, string p) {
-        int index1 = s.size();
-        int index2 = p.size();
+ 
+        int n = s.size();
+        int m = p.size();
 
-        vector<vector<int>> dp(index1,vector<int>(index2,-1));
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
 
-        return Wildcard_matching(index1-1,index2-1,s,p,dp);
+        
+
+        dp[0][0] = 1;
+        for(int index2=1;index2<=m;index2++){
+            int flag = true;
+            for(int i=1; i<=index2;i++ ){
+            if(p[i-1] != '*'){
+                flag = false;
+            }
+            }
+            dp[0][index2] = flag;
+        }
+        for(int index1=1;index1<=n;index1++){
+            for(int index2=1;index2<=m;index2++){
+                if(s[index1-1] == p[index2-1] || p[index2-1] == '?'){
+                    dp[index1][index2]=dp[index1-1][index2-1];
+                }else if(p[index2-1] == '*'){
+                    dp[index1][index2] = dp[index1-1][index2] || dp[index1][index2-1]; 
+                }else{
+                    dp[index1][index2] = false;
+                }
+            }
+        }
+
+        return dp[n][m];
+
 
     }
 };
