7ca970d9d9e1f0b90fc93219e85a651ea0f82986
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-20 00:43:24 +0530

    Time: 1711 ms (18.65%) | Memory: 63 MB (9.07%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+bool ispalindrome(int i,int j,string &s){
+
+    while(i <= j){
+        if(s[i] != s[j]){
+            return false;
+           
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+int Palindrome_partitioning_II(int i,int j,string &s,vector<vector<int>> &dp){
+    if(i > j){
+        return 0;
+    }
+    if(ispalindrome(i,j,s)){
+        return 0;
+    }
+    if(dp[i][j] != -1){
+        return dp[i][j];
+    }
+    int mini = INT_MAX;
+
+    for(int k=i;k<=j;k++){
+        if(ispalindrome(i,k,s)){
+              int ans = 1  + Palindrome_partitioning_II(k+1,j,s,dp);
+               mini = min(ans,mini);
+        }
+       
+    }
+    return dp[i][j]=mini;
+}
+public:
+    int minCut(string s) {
+            		
+        int n = s.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+
+        return Palindrome_partitioning_II(0,n-1,s,dp);
+    }
+};
