a18f2b93a503ffaf6f25aba3633f9f85294589bc
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-20 21:20:11 +0530

    Time: 4 ms (72.14%) | Memory: 12.5 MB (31.04%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -1,30 +1,26 @@
 class Solution {
-int Partition_Array_for_Maximum_Sum(int i,vector<int>& arr, int k,vector<int> &dp){
-    if(i == arr.size()){
-        return 0;
-    }
-    if(dp[i] != -1){
-        return dp[i];
-    }
-    int maxi = INT_MIN;
-    int curr_max = INT_MIN;
-    for(int m = i;m<min((int)arr.size(),i+k);m++){
-        curr_max = max(arr[m],curr_max);
-        if((m-i)+1 <= k){
-            int ans = (curr_max * ((m-i)+1)) + Partition_Array_for_Maximum_Sum(m+1,arr,k,dp);
 
-            maxi = max(maxi,ans);
-        }
-    }
-    return dp[i] = maxi;
-}
 
 public:
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
         int n = arr.size();
-        vector<int> dp(n,-1);
+        vector<int> dp(n+1,0);
 
-        return Partition_Array_for_Maximum_Sum(0,arr,k,dp);
+        for(int i=n-1;i>=0;i--){
+            int maxi = INT_MIN;
+            int curr_max = INT_MIN;
+            for(int m = i;m<min((int)arr.size(),i+k);m++){
+                curr_max = max(arr[m],curr_max);
+                if((m-i)+1 <= k){
+                int ans = (curr_max * ((m-i)+1)) + dp[m+1];
+
+                maxi = max(maxi,ans);
+                }
+            }
+            dp[i] = maxi;
+        }
+
+        return dp[0];
 
     }
 };
