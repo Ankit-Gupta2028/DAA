7c17284e667a445c7c155f0209fa14d93285afc6
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-21 19:55:03 +0530

    Time: 7 ms (65.82%) | Memory: 19.2 MB (75.51%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,61 +1,67 @@
 class Solution {
-private:
-    int largest_histro_rec(vector<int> &arr){
-        int n=arr.size();
-        int left=0,right=0;
+int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
         stack<int> s1;
-        int max_rect=0;
-        for(int i=0;i<n;i++){
-            int curr=arr[i];
-            while(!s1.empty() && arr[s1.top()]>curr){
-                int index=s1.top();
-                s1.pop();
-                int right=i;
-                if(s1.empty()){
-                    left=-1;
-                }else{
-                    left=s1.top();
-                }
-                max_rect=max(max_rect,arr[index]*(right-left-1));
+        int left=0,right=0;
+        int LRH=0;
 
+
+        for(int i=0;i<n;i++){
+            int curr=heights[i];
+            while(!s1.empty() && curr <= heights[s1.top()]){
+
+                int index_pop_ele=s1.top();
+                s1.pop();
+                right=i;
+
+                if(s1.empty()){
+                    left= -1;
+                }else{
+                    left= s1.top();
+                }
+
+                LRH=max(LRH,(heights[index_pop_ele]*(right-left-1)));
+              
             }
             s1.push(i);
         }
         while(!s1.empty()){
-                int index=s1.top();
-                s1.pop();
-                int right=n;
-                if(s1.empty()){
-                    left=-1;
-                }else{
-                    left=s1.top();
-                }
-                max_rect=max(max_rect,arr[index]*(right-left-1));
+
+            int index_pop_ele=s1.top();
+            s1.pop();
+            right=n;
+
+            if(s1.empty()){
+                left=-1;
+            }else{
+                left=s1.top();
+            }
+
+            LRH=max(LRH,(heights[index_pop_ele]*(right-left-1)));
         }
-        return max_rect;
+        return LRH;
     }
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int max_rectangular=0;
-        int m=matrix[0].size();
-        int n=matrix.size();
-        vector<int> histograms(m,0);
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int> curr_size(m,0);
+        int ans ;
+        ans = INT_MIN;
+      
 
         for(int i=0;i<n;i++){
-
             for(int j=0;j<m;j++){
-                if(matrix[i][j]-'0' == 1){
-                    histograms[j]+=1;
-                   
+                if(matrix[i][j] == '1'){
+                    curr_size[j] +=1 ;
                 }else{
-
-                    histograms[j]=0;
+                    curr_size[j] = 0 ; 
                 }
             }
-            max_rectangular=max(max_rectangular,largest_histro_rec(histograms));
+            ans = max(ans,largestRectangleArea(curr_size)) ;
         }
-       
+        return ans;
 
-        return max_rectangular;
     }
 };
