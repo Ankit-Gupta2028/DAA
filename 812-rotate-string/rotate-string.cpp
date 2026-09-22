7b2a99898289f2cbe44cee34f7b1e0c820e746f3
Ankit Gupta <iamankit.9798@gmail.com>
2026-09-23 00:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (83.38%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+           if(s.size() != goal.size()){
+                return false;
+            }
+
+            string double_s = s+s;
+            if(double_s.find(goal) == string::npos) {
+                return false;
+            }
+            return true;
+    }
+};
